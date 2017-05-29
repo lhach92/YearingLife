593d3224329ac2fc425d1aa4339d457f4db287b9
Henry L <lhach92@163.com>
2017-05-30 00:00:59 +0800

    Initial Commit

---
+++ YearningLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YearningLife
+//
+//  Created by boxfish on 2017/5/30.
+//  Copyright © 2017年 LH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
